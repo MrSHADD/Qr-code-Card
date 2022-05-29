34af2772ec033212ad8b53adef10e83f7ea6999d
Israel Oluwafemi Alabi <israelalabi97@yahoo.com>
2022-05-29 19:19:55 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Qrcode from "./components/Qrcode"
+
+export default function App(){
+    return(
+        <div className="div--app">
+            <Qrcode/>
+        </div>
+        
+    )
+}
---
+++ src/components/Qrcode.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function(){
+    return(
+        <div>
+           <img src="./images/image-qr-code.png" className="qr--img"/> 
+           <h2>Improve your front-end skills by building projects</h2>
+           <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+
+        </div>
+    )
+}
