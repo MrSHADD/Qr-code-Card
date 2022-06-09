2aac134b89cc278ad482f3a1acf4c97bcdbe7089
Israel Oluwafemi Alabi <israelalabi97@yahoo.com>
2022-06-09 01:46:33 +0100

    Update Qrcode.js

--- src/components/Qrcode.js
+++ src/components/Qrcode.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function(){
     return(
         <div>
-           <img src="./images/image-qr-code.png" className="qr--img"/> 
+           <img src="./images/image-qr-code.png" className="qr--img" alt="Qr image"/> 
            <h2>Improve your front-end skills by building projects</h2>
            <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
 
